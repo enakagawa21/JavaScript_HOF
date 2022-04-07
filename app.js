5d27fcbcc2d2bedf1ece2256c3499b1090c58e43
EricN <eric_nakagawa@yahoo.com>
2022-04-07 09:10:02 -0700

    HOF exercise done

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+
+
+function plus(num){
+return function (sum){
+    return num + sum;
+}
+
+}
+const plus100 = plus(100)
+const plus5 = plus(1)
+
+console.log(plus100(100))
+console.log(plus5 (5))
+console.log(plus100(4589))
+
